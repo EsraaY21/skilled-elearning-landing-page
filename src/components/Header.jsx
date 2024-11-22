128df1b07d9842d286800b4d8c462fbde79076dc
Esra <esraayaareb@gmail.com>
2024-11-22 20:20:30 +0300

    hero

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import LogoDark from "../assets/logo-dark.svg";
+import DesktopHero from "../assets/image-hero-desktop.png";
 
 export default function Header() {
   return (
@@ -14,15 +15,24 @@ export default function Header() {
           </div>
         </div>
       </nav>
+
+      <section className="hero_section">
+        <div className="container hero">
+          <div className="hero_content">
+            <h1 className="text-1">Maximize skill, minimize budget</h1>
+            <p className="text-5">
+              Our modern courses across a range of in-demand skills will give
+              you the knowledge you need to live the life you want.
+            </p>
+            <a href="#" className="btn text-7 gradient-1">
+              Get Started
+            </a>
+          </div>
+          <div className="image">
+            {/* <img src={DesktopHero} className="" alt="Desktop Hero" /> */}
+          </div>
+        </div>
+      </section>
     </header>
   );
 }
-
-// Maximize skill, minimize budget
-
-// Our modern courses across a range of in-demand skills will give you the
-// knowledge you need to live the life you want.
-
-// Get Started
-
-//
